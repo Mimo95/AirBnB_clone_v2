a156efadc7ad82980b93a43d435e32f32c22b17e
mohamed hajjoubi <mh041949@gmail.com>
2024-04-16 15:10:31 -0700

    Add test cases for create

---
+++ tests/test_console.py
@@ -0,0 +1,51 @@
+from io import StringIO
+import unittest
+from unittest.mock import patch
+from console import HBNBCommand
+
+class TestConsoleCommands(unittest.TestCase):
+    def test_do_create_user(self):
+        """Test do_create method with User class"""
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            HBNBCommand().onecmd("create User name=\"anas\"")
+            output = mock_stdout.getvalue().strip()
+            self.assertTrue(len(output) == 36)  
+
+        
+    def test_do_create_place(self):
+        """Test do_create method with Place class"""
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            HBNBCommand().onecmd("create Place city_id=\"123\" user_id=\"456\" name=\"my_place\"")
+            output = mock_stdout.getvalue().strip()
+            self.assertTrue(len(output) == 36)  
+
+    def test_do_create_state(self):
+        """Test do_create method with State class"""
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            HBNBCommand().onecmd("create State name=\"California\"")
+            output = mock_stdout.getvalue().strip()
+            self.assertTrue(len(output) == 36)  
+
+    def test_do_create_city(self):
+        """Test do_create method with City class"""
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            HBNBCommand().onecmd("create City state_id=\"789\" name=\"San Francisco\"")
+            output = mock_stdout.getvalue().strip()
+            self.assertTrue(len(output) == 36)  
+
+    def test_do_create_amenity(self):
+        """Test do_create method with Amenity class"""
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            HBNBCommand().onecmd("create Amenity name=\"WiFi\"")
+            output = mock_stdout.getvalue().strip()
+            self.assertTrue(len(output) == 36)  
+
+    def test_do_create_review(self):
+        """Test do_create method with Review class"""
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            HBNBCommand().onecmd("create Review place_id=\"111\" user_id=\"222\" text=\"Great place!\"")
+            output = mock_stdout.getvalue().strip()
+            self.assertTrue(len(output) == 36)  
+
+if __name__ == '__main__':
+    unittest.main()
