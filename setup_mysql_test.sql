b28547d7aab4314d3ef766990952f92f38c0c895
mohamed hajjoubi <mh041949@gmail.com>
2024-04-16 15:36:51 -0700

    create DBtest

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- script that creates the database hbnb_dev_db and the user hbnb_test
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
