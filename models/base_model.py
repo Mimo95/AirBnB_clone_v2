d295d20dbf7ecf2b3d2e2cf09a9d5d30a84d1d22
mohamed hajjoubi <mh041949@gmail.com>
2024-04-16 16:16:28 -0700

    Update BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from models import storage
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
 
@@ -46,6 +45,7 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
+        from models import storage
         self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
@@ -67,4 +67,5 @@ class BaseModel:
     def delete(self):
         """Delete current instance from storage
         """
+        from models import storage
         storage.delete(self)
